7ba6e810dc3ea025e3758e0acc183066d980b068
YE-EUN <musiclove529@naver.com>
2024-07-24 11:32:06 +0900

    데이터 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import DefaultLayout from "./layouts/DefaultLayout";
 import TodoHeader from "./components/todos/TodoHeader";
 import TodoBody from "./components/todos/TodoBody";
+import { useState } from "react";
+
+const dummyTodos = [
+  {
+    id: 1,
+    title: "React 공부",
+    summary: "React를 공부한다.",
+    category: "TODO",
+  },
+  {
+    id: 2,
+    title: "점심 먹기",
+    summary: "점심을 먹는다.",
+    category: "PROGRESS",
+  },
+  {
+    id: 3,
+    title: "커피 마시기",
+    summary: "커피를 마신다.",
+    category: "DONE",
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(dummyTodos);
+
   return (
     <>
       <DefaultLayout>
@@ -32,7 +56,7 @@ function App() {
             <TodoHeader />
           </div>
           <div>
-            <TodoBody />
+            <TodoBody todos={todos} />
           </div>
         </section>
       </DefaultLayout>
--- src/components/todos/TodoBody.jsx
+++ src/components/todos/TodoBody.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const todos = [1, 2, 3];
-
-const TodoBody = () => {
+const TodoBody = ({ todos }) => {
   return (
     <ul className="px-0 my-8">
       {/* li태그를 todos 배열의 요소만큼 렌더링 */}
       {todos.map((todo) => (
-        <TodoItem key={todo} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} />
       ))}
     </ul>
   );
